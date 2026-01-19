67f213ffe4f7b79ffe410cf9b8a0b235cb4c2c99
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2026-01-19 02:24:11 -0300

    se agrego el metodo Buscar

--- SistemaVentasSimple/Negocio/ClienteNegocio.cs
+++ SistemaVentasSimple/Negocio/ClienteNegocio.cs
@@ -34,6 +34,9 @@ namespace Negocio
 
                 return lista;
             }
+            catch(Exception ex){
+                throw new Exception("Error al listar los clientes", ex);
+            }
             finally 
             {
                 datos.cerrarConexion();
@@ -55,6 +58,10 @@ namespace Negocio
                 datos.setearParametro("@telefono", cliente.Telefono);
                 datos.setearParametro("@estado", cliente.Estado);
                 datos.ejecutarAccion();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error al agregar el cliente", ex);
             }
             finally
             {
@@ -78,6 +85,10 @@ namespace Negocio
                 datos.setearParametro("@id", cliente.IdCliente);
                 datos.ejecutarAccion();
             }
+            catch(Exception ex)
+            {
+                throw new Exception("Error al modificar el cliente", ex);
+            }
             finally
             {
                 datos.cerrarConexion();
@@ -94,6 +105,10 @@ namespace Negocio
                 datos.setearConsulta(query);
                 datos.setearParametro("@id", id);
                 datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar eliminar al cliente", ex);
             }
             finally
             {
@@ -118,6 +133,49 @@ namespace Negocio
                 }
 
                 return false;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error al intentar verificar la existencia del cliente", ex );
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public List<Cliente> Buscar(string dni)
+        {
+            List<Cliente> lista = new List<Cliente>();
+            AccesoDatos datos = new AccesoDatos();
+            string query = "SELECT IdCliente, Nombre, Apellido, Dni, Telefono, Email, Estado FROM Clientes WHERE Estado = 1 AND Dni LIKE '%@filtro%';";
+
+            try
+            {
+                datos.setearConsulta(query);
+                datos.setearParametro("@filtro", dni);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Cliente aux = new Cliente();
+                    aux.IdCliente = datos.Lector["IdCliente"] is DBNull ? 0 : (int)datos.Lector["IdCliente"];
+                    aux.Nombre = datos.Lector["Nombre"] is DBNull ? "" : (string)datos.Lector["Nombre"];
+                    aux.Apellido = datos.Lector["Apellido"] is DBNull ? "" : (string)datos.Lector["Apellido"];
+                    aux.Dni = datos.Lector["Dni"] is DBNull ? "" : (string)datos.Lector["Dni"];
+                    aux.Email = datos.Lector["Email"] is DBNull ? "" : (string)datos.Lector["Email"];
+                    aux.Telefono = datos.Lector["Telefono"] is DBNull ? "" : (string)datos.Lector["Telefono"];
+                    aux.Estado = datos.Lector["Estado"] is DBNull ? false : (bool)datos.Lector["Estado"];
+
+                    lista.Add(aux);
+                }
+
+
+                return lista;
+            }
+            catch(Exception ex) 
+            {
+                throw new Exception("Error al buscar cliente por dni", ex);
             }
             finally
             {
